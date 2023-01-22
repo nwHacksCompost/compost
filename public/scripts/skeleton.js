adc047f1434c63c0b4012f714d210473f3c0a894
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-01-22 00:59:29 -0800

    first push

--- public/scripts/skeleton.js
+++ public/scripts/skeleton.js
@@ -1,6 +1,11 @@
 "use strict";
-function loadSkeleton(){
+function loadSkeleton() {
     console.log($('#navPlaceholder').load('../../app/html/nav.html'));
     console.log($('#footerPlaceholder').load('../../app/html/footer.html'));
 }
 loadSkeleton();
+
+$(".goBack").on("click", function (e) {
+    e.preventDefault();
+    window.history.back();
+});
